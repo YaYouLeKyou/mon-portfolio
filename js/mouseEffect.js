f55a35cc9598e3c9e687e4d51dddba0c7ee970a0
yann Hadiouche <78508960+YaYouLeKyou@users.noreply.github.com>
2023-01-09 14:28:13 +0100

    Add files via upload

---
+++ js/mouseEffect.js
@@ -0,0 +1,105 @@
+//Mouse effect for carousel page
+document.getElementById('project1').addEventListener('mouseover', mouseOver);
+document.getElementById('project1').addEventListener('mouseout', mouseOut);
+
+function mouseOver() {
+  document.getElementById('screen1').style.visibility = 'visible';
+  document.getElementById('screen1').style.transition = '0.8s';
+  document.getElementById('screen1').style.opacity = '1';
+}
+
+function mouseOut() {
+  document.getElementById('screen1').style.visibility = 'hidden';
+  document.getElementById('screen1').style.transition = '0.8s';
+  document.getElementById('screen1').style.opacity = '0';
+}
+
+document.getElementById('project2').addEventListener('mouseover', mouseOver2);
+document.getElementById('project2').addEventListener('mouseout', mouseOut2);
+
+function mouseOver2() {
+  document.getElementById('screen2').style.visibility = 'visible';
+  document.getElementById('screen2').style.transition = '0.8s';
+  document.getElementById('screen2').style.opacity = '1';
+}
+
+function mouseOut2() {
+  document.getElementById('screen2').style.visibility = 'hidden';
+  document.getElementById('screen2').style.transition = '0.8s';
+  document.getElementById('screen2').style.opacity = '0';
+}
+
+document.getElementById('project3').addEventListener('mouseover', mouseOver3);
+document.getElementById('project3').addEventListener('mouseout', mouseOut3);
+
+function mouseOver3() {
+  document.getElementById('screen3').style.visibility = 'visible';
+  document.getElementById('screen3').style.transition = '0.8s';
+  document.getElementById('screen3').style.opacity = '1';
+}
+
+function mouseOut3() {
+  document.getElementById('screen3').style.visibility = 'hidden';
+  document.getElementById('screen3').style.transition = '0.8s';
+  document.getElementById('screen3').style.opacity = '0';
+}
+
+document.getElementById('project4').addEventListener('mouseover', mouseOver4);
+document.getElementById('project4').addEventListener('mouseout', mouseOut4);
+
+function mouseOver4() {
+  document.getElementById('screen4').style.visibility = 'visible';
+  document.getElementById('screen4').style.transition = '0.8s';
+  document.getElementById('screen4').style.opacity = '1';
+}
+
+function mouseOut4() {
+  document.getElementById('screen4').style.visibility = 'hidden';
+  document.getElementById('screen4').style.transition = '0.8s';
+  document.getElementById('screen4').style.opacity = '0';
+}
+
+document.getElementById('project5').addEventListener('mouseover', mouseOver5);
+document.getElementById('project5').addEventListener('mouseout', mouseOut5);
+
+function mouseOver5() {
+  document.getElementById('screen5').style.visibility = 'visible';
+  document.getElementById('screen5').style.transition = '0.8s';
+  document.getElementById('screen5').style.opacity = '1';
+}
+
+function mouseOut5() {
+  document.getElementById('screen5').style.visibility = 'hidden';
+  document.getElementById('screen5').style.transition = '0.8s';
+  document.getElementById('screen5').style.opacity = '0';
+}
+
+document.getElementById('project6').addEventListener('mouseover', mouseOver6);
+document.getElementById('project6').addEventListener('mouseout', mouseOut6);
+
+function mouseOver6() {
+  document.getElementById('screen6').style.visibility = 'visible';
+  document.getElementById('screen6').style.transition = '0.8s';
+  document.getElementById('screen6').style.opacity = '1';
+}
+
+function mouseOut6() {
+  document.getElementById('screen6').style.visibility = 'hidden';
+  document.getElementById('screen6').style.transition = '0.8s';
+  document.getElementById('screen6').style.opacity = '0';
+}
+
+document.getElementById('project7').addEventListener('mouseover', mouseOver7);
+document.getElementById('project7').addEventListener('mouseout', mouseOut7);
+
+function mouseOver7() {
+  document.getElementById('screen7').style.visibility = 'visible';
+  document.getElementById('screen7').style.transition = '0.8s';
+  document.getElementById('screen7').style.opacity = '1';
+}
+
+function mouseOut7() {
+  document.getElementById('screen7').style.visibility = 'hidden';
+  document.getElementById('screen7').style.transition = '0.8s';
+  document.getElementById('screen7').style.opacity = '0';
+}
