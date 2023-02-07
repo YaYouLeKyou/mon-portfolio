a201d875f4417cf24a429510445f339481f7a39a
yann Hadiouche <78508960+YaYouLeKyou@users.noreply.github.com>
2023-02-07 13:05:11 +0100

    Update typing-anim.js

--- js/typing-anim.js
+++ js/typing-anim.js
@@ -478,7 +478,7 @@
 
 $(function () {
   $('#quote').typed({
-    strings: ['I am Yanès Hadiouche Developer front-end'],
+    strings: ['I am Yanès Hadiouche Developer web front-end'],
     typeSpeed: 50,
   });
 });
